1256730b3049d11f9c40e93efa76688e76a4b0fc
Philip Nguyen <supertaru@gmail.com>
2014-08-20 21:55:39 -0700

    Finish visualization of bubble sort and update to readme

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 $(function(){
-  var stickHeight = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20].reverse();
 
-  var maxSticks = stickHeight.length;
+  var stickHeight = []
+
+  var maxSticks;
 
   var i = 1;
 
   var didSort = false;
 
+  function fillStickHeight () {
+    var arr = [];
+    for (var x = 0; x < 50; x++){
+      stickHeight.push(Math.floor(Math.random() * 50 + 20));
+    }
+    maxSticks = stickHeight.length;
+  }
+
   //initiate sticks
   function createSticks() {
-    for (var n = 0; n < maxSticks; n++ ) {
+    for (var n = 0; n < stickHeight.length; n++ ) {
       $("#sort-area").append("<div class='stick' data-height=" + stickHeight[n] + " style='height:" + stickHeight[n] + "px'></div>");
     }
   }
@@ -20,30 +29,29 @@ $(function(){
   }
 
   function goSort() {
-    if (i < maxSticks) {
-      var beforeStick = stickHeight[i - 1];
-      var afterStick = stickHeight[i];
-      if (beforeStick > afterStick) {
-        var afterTemp = afterStick;
-        stickHeight[i] = beforeStick;
-        stickHeight[i - 1] = afterTemp;
-        didSort = true;
-      }
-      insertSticks();
+    var beforeStick = stickHeight[i - 1];
+    var afterStick = stickHeight[i];
+    if (beforeStick > afterStick) {
+      var afterTemp = afterStick;
+      stickHeight[i] = beforeStick;
+      stickHeight[i - 1] = afterTemp;
+      didSort = true;
+    }
+    insertSticks();
 
-      if (i === maxSticks - 1 && didSort === false){
-        alert('done!');
-      }
-      else {
-        startSort();
-      }
-      if (i === maxSticks - 1){
-        i = 1;
-        didSort = false;
-      }
-      else {
-        i++;
-      }
+    if (i === maxSticks - 1 && didSort === false){
+      alert('done!');
+    }
+    else {
+      startSort();
+    }
+    if (i === maxSticks - 1){
+      i = 1;
+      maxSticks--;
+      didSort = false;
+    }
+    else {
+      i++;
     }
   }
 
@@ -51,6 +59,8 @@ $(function(){
     window.setTimeout(goSort, 100);
   }
 
+  fillStickHeight();
   createSticks();
-  startSort();
+
+  $('.sort-button').on('click', startSort);
 });
